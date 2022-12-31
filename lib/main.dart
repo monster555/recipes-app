c4d273dddbb48943767c79cb0932b115021b8ff0
Daniel Coyula <monsterdrummer5@gmail.com>
2022-12-31 12:44:27 -0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:flutter/services.dart';
 import 'package:recipes_app/src/presentation/app.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   // Restrict device orientation to portrait to avoid screen rotation
   await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
 
